3f07fb7bbb96f7996b19d4d3e845fde8bd2f72ff
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-12-14 06:44:13 +0100

    fix(sqitch/team): remove not null constraint on charity organization id

--- sqitch/deploy/table_team.sql
+++ sqitch/deploy/table_team.sql
@@ -11,7 +11,7 @@ CREATE TABLE trapparty.team (
   event_id                   BIGSERIAL NOT NULL REFERENCES trapparty.event(id),
   name                       TEXT NOT NULL UNIQUE CHECK (char_length(name) < 100),
   emoji                      TEXT NOT NULL UNIQUE CHECK (char_length(emoji) = 1),
-  charity_organization_id    BIGSERIAL NOT NULL REFERENCES trapparty.charity_organization(id),
+  charity_organization_id    BIGSERIAL REFERENCES trapparty.charity_organization(id),
   donation_url               TEXT CHECK (char_length(donation_url) < 100 AND donation_url ~ '^https://.+$')
 );
 
