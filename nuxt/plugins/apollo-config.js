bfd60db00533338febe6102db71938df4b2afafd
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-12-14 12:56:53 +0100

    fix(apollo): correct service address

--- nuxt/plugins/apollo-config.js
+++ nuxt/plugins/apollo-config.js
@@ -1,8 +1,8 @@
 export default (_ctx) => {
   return {
     httpEndpoint: process.server
-      ? 'http://trapparty_postgraphile:5000/graphql'
-      : 'https://trapparty_postgraphile.' +
+      ? 'http://trapparty-postgraphile:5000/graphql'
+      : 'https://trapparty-postgraphile.' +
         (process.env.NUXT_ENV_STACK_DOMAIN || 'jonas-thelemann.test') +
         '/graphql',
   }
