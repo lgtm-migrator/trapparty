39e0b10eec536178f3db070b2c714f38f2e6f37b
Jonas Thelemann <e-mail@jonas-thelemann.de>
2021-02-21 17:32:09 +0100

    feat(eslint): remove unneeded prettier config

--- nuxt/.eslintrc.js
+++ nuxt/.eslintrc.js
@@ -5,12 +5,9 @@ module.exports = {
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
-    'prettier',
-    'prettier/vue',
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
   ],
-  plugins: ['prettier'],
   // add your custom rules here
   root: true,
   rules: {},
