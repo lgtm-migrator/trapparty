2bfe38caa27a5dc6e08d02ed2f4efeec1fe5d17e
Jonas Thelemann <e-mail@jonas-thelemann.de>
2022-04-16 05:08:18 +0200

    feat(error): upgrade status-i18n

--- nuxt/layouts/error.vue
+++ nuxt/layouts/error.vue
@@ -2,17 +2,22 @@
   <Error :status-code="+error.statusCode" />
 </template>
 
-<script>
-export default {
-  name: 'MaevsiError',
+<script lang="ts">
+import { defineComponent, PropType } from '#app'
+
+type Error = { statusCode: number }
+
+export default defineComponent({
+  name: 'TrapPartyError',
   props: {
     error: {
-      type: Object,
+      type: Object as PropType<Error | undefined>,
       default: undefined,
     },
   },
   head() {
-    return { title: this.error.statusCode }
+    const error = this.error as Error | undefined
+    return { title: error?.statusCode?.toString() }
   },
-}
+})
 </script>
